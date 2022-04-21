d9261e0be18af31fbdbfb1076a958b69511e045e
DiegoRojo99 <diego99rojo@gmail.com>
2022-04-21 12:49:31 +0200

    Player Created

--- index.js
+++ index.js
@@ -10,8 +10,20 @@ c.fillRect(0,0,canvas.width,canvas.height)
 
 const image = new Image()
 image.src='./img/PelletTown.png'
-console.log(image)
+
+const playerImage = new Image()
+playerImage.src='./img/playerDown.png'
 
 image.onload = () => {  
-    c.drawImage(image, -700, -550)
+    c.drawImage(image, -785, -650)
+    c.drawImage(playerImage, 
+        0,
+        0,
+        playerImage.width / 4,
+        playerImage.height,
+        canvas.width / 2 - playerImage.width / 4 , 
+        canvas.height / 2 - playerImage.height / 2,
+        playerImage.width / 4,
+        playerImage.height
+    )
 }
