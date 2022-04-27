01fd2528e3ecef53b5c2aed7bcd2e3f60ac0bbfb
DiegoRojo99 <diego99rojo@gmail.com>
2022-04-27 15:36:22 +0200

    There is a dialogue box that shows the attack

--- index.js
+++ index.js
@@ -286,68 +286,6 @@ function animate(){
 }
 
 
-const battleBattlegroundImage = new Image()
-battleBattlegroundImage.src = './img/battleBackground.png'
-const battleBackground = new Sprite({
-    position: {
-        x: 0,
-        y: 0
-    }, image: battleBattlegroundImage
-})
-
-const draggleImage = new Image()
-draggleImage.src = './img/draggleSprite.png'
-const draggle = new Sprite({
-    position: {
-        x: 0800,
-        y: 100
-    }, image: draggleImage,
-    frames:{
-        max: 4, hold: 30
-    },
-    animate: true,
-    isEnemy: true
-})
-const embyImage = new Image()
-embyImage.src = './img/embySprite.png'
-const emby = new Sprite({
-    position: {
-        x: 280,
-        y: 325
-    }, image: embyImage,
-    frames:{
-        max: 4, hold: 30
-    },
-    animate: true
-})
-
-const renderedSprites = [draggle, emby]
-function animateBattle(){
-    window.requestAnimationFrame(animateBattle)
-    battleBackground.draw()
-    draggle.draw()
-    emby.draw()
-
-    renderedSprites.forEach((sprite) => {
-        sprite.draw()
-    })
-}
-
-
-//animate()
-animateBattle()
-
-
-document.querySelectorAll('button').forEach((button) => {
-    button.addEventListener('click', (e) => {
-        const selectedAttack= attacks[e.currentTarget.innerHTML]
-        emby.attack({ 
-            attack: selectedAttack, 
-            recipient: draggle,
-            renderedSprites
-        })
-    })
-})
 
 lastKey=''
 window.addEventListener('keydown',(e) => {
